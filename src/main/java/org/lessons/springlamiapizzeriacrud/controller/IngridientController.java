7940f3dcd1a95a5a310b02cf074e24ba448f8552
FontaMatte <matteo.fontanel@gmail.com>
2023-06-30 17:03:54 +0200

    Add Edit function for Ingredients

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngridientController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/IngridientController.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ingridients")
@@ -21,10 +19,21 @@ public class IngridientController {
     private IngridientRepository ingridientRepository;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, @RequestParam("edit") Optional<Integer> ingridientId) {
         List<Ingridient> ingridientList = ingridientRepository.findAll();
         model.addAttribute("ingridients", ingridientList);
-        model.addAttribute("ingridientObj", new Ingridient());
+        Ingridient ingridientObj;
+        if (ingridientId.isPresent()) {
+            Optional<Ingridient> ingridientDb = ingridientRepository.findById(ingridientId.get());
+            if (ingridientDb.isPresent()) {
+                ingridientObj = ingridientDb.get();
+            } else {
+                ingridientObj = new Ingridient();
+            }
+        } else {
+            ingridientObj = new Ingridient();
+        }
+        model.addAttribute("ingridientObj", ingridientObj);
         return "/ingridients/index";
     }
 
